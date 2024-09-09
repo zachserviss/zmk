d06d5bf67ef48c35751b03f3a2b81dbca05dd60f
Zach Serviss <Zachary.Serviss@gov.ba.ca>
2024-09-08 20:19:26 -0700

    random image implementation

--- app/boards/shields/nice_view/widgets/peripheral_status.c
+++ app/boards/shields/nice_view/widgets/peripheral_status.c
@@ -119,8 +119,7 @@ int zmk_widget_status_init(struct zmk_widget_status *widget, lv_obj_t *parent) {
     lv_canvas_set_buffer(top, widget->cbuf, CANVAS_SIZE, CANVAS_SIZE, LV_IMG_CF_TRUE_COLOR);
 
     lv_obj_t *art = lv_img_create(widget->obj);
-    bool random = sys_rand32_get() & 1;
-    lv_img_set_src(art, &vamp);
+    lv_img_set_src(art, (lv_img_dsc_t *[]){&test, &re8, &vamp, &house}[sys_rand32_get() % 4]);
     lv_obj_align(art, LV_ALIGN_TOP_LEFT, 0, 0);
 
     sys_slist_append(&widgets, &widget->node);
