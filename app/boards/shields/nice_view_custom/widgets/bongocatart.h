5c7857e697e0203f58cdc5b54dbf59054879f8e3
Zach Serviss <zacharyserviss@proton.me>
2025-03-05 17:32:49 -0800

    update

--- app/boards/shields/nice_view_custom/widgets/bongocatart.h
+++ app/boards/shields/nice_view_custom/widgets/bongocatart.h
@@ -3,10 +3,12 @@
 
 #include <lvgl.h>
 
-extern const lv_img_dsc_t bongocatrest0;
-extern const lv_img_dsc_t bongocatcasual1;
-extern const lv_img_dsc_t bongocatcasual2;
-extern const lv_img_dsc_t bongocatfast1;
-extern const lv_img_dsc_t bongocatfast2;
+extern const lv_img_dsc_t bongo_resting;
+extern const lv_img_dsc_t bongo_casualright;
+extern const lv_img_dsc_t bongo_casualleft;
+extern const lv_img_dsc_t bongo_furiousup;
+extern const lv_img_dsc_t bongo_furiousdown;
+extern const lv_img_dsc_t bongo_inhale;
+extern const lv_img_dsc_t bongo_exhale;
 
-#endif /* BONGOCATART_H */ 
+#endif /* BONGOCATART_H */
